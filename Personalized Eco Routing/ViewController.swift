83703c53b6c953e67a25377eff3efb044e08f293
vidur2 <vmod2005@gmail.com>
2022-05-24 12:57:45 -0400

    thing

--- Personalized Eco Routing/ViewController.swift
+++ Personalized Eco Routing/ViewController.swift
@@ -25,16 +25,16 @@ class ViewController: UIViewController {
          
         // Request a route using MapboxDirections.swift
         Directions.shared.calculate(routeOptions) { [weak self] (session, result) in
-        switch result {
-            case .failure(let error):
-                print(error.localizedDescription)
-            case .success(let response):
-                guard let self = self else { return }
-                // Pass the first generated route to the the NavigationViewController
-                let viewController = NavigationViewController(for: response, routeIndex: 0, routeOptions: routeOptions)
-                viewController.modalPresentationStyle = .fullScreen
-                self.present(viewController, animated: true, completion: nil)
-        }
+            switch result {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                case .success(let response):
+                    guard let self = self else { return }
+                    // Pass the first generated route to the the NavigationViewController
+                    let viewController = NavigationViewController(for: response, routeIndex: 0, routeOptions: routeOptions)
+                    viewController.modalPresentationStyle = .fullScreen
+                    self.present(viewController, animated: true, completion: nil)
+            }
         }
     }
 }

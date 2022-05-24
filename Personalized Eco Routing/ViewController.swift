744e24ebae82baa77158941fb667246909e6d3cd
vidur2 <vmod2005@gmail.com>
2022-05-24 12:24:16 -0400

    first one

--- Personalized Eco Routing/ViewController.swift
+++ Personalized Eco Routing/ViewController.swift
@@ -5,15 +5,37 @@
 //  Created by Vidur Modgil on 5/24/22.
 //
 
+
 import UIKit
-
+import MapboxDirections
+import MapboxCoreNavigation
+import MapboxNavigation
+import CoreLocation
+ 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         
+        // Define two waypoints to travel between
+        let origin = Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.9131752, longitude: -77.0324047), name: "Mapbox")
+        let destination = Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.8977, longitude: -77.0365), name: "White House")
+         
+        // Set options
+        let routeOptions = NavigationRouteOptions(waypoints: [origin, destination])
+         
+        // Request a route using MapboxDirections.swift
+        Directions.shared.calculate(routeOptions) { [weak self] (session, result) in
+        switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let response):
+                guard let self = self else { return }
+                // Pass the first generated route to the the NavigationViewController
+                let viewController = NavigationViewController(for: response, routeIndex: 0, routeOptions: routeOptions)
+                viewController.modalPresentationStyle = .fullScreen
+                self.present(viewController, animated: true, completion: nil)
+        }
+        }
     }
-
-
 }
 
